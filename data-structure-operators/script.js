3509c11c5387d0089d81908f562c659270e0c5dd
Kean Duque <vizen27@users.noreply.github.com>
2021-04-11 17:39:57 +0800

    challenge2
    challenge 2 and script structures

--- data-structure-operators/script.js
+++ data-structure-operators/script.js
@@ -4,6 +4,23 @@
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  // [`day-${2 + 4}`]: {
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: "Classico Italiano",
@@ -12,49 +29,113 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  //ES6 enhanced object Literals
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  //ES6 Enhancement you can remove the function in object see sample below
   //using Destructuring Object
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = "20:00",
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     // using Spread unpacking elements
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     //using REST operator arguments
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
+/* 
+//////////////////////////////
+//Looping Objects : Object.keys, values, and entries
+
+//Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+//Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire Object with Keys and Values
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+ */
+/* 
+//////////////////////////////
+//Optional Chaining (?.)
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  //console.log(restaurant.openingHours.mon);
+  //if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
+
+  //WITH optional chaining
+  console.log(restaurant.openingHours.mon?.open); //undefined
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  //console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+//Arrays
+const usersArr = [{ name: "Kean", email: "kduque@yahoo.com" }];
+
+//optional chaining
+console.log(usersArr[0]?.email ?? "User array empty");
+
+//equivalent to
+if (usersArr.length > 0) console.log(usersArr[0].name);
+else console.log("user array empty"); */
+
+/* 
+//////////////////////////////
+//Looping Arrays the For Of Loop ES6
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+
+//.entries() return the index number and element itself
+//old way
+for (const item of menu.entries()) {
+  //console.log(`${item[0] + 1}: ${item[1]}`); // old way
+}
+//new way with destructuring
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+} */
+
+//console.log([...menu.entries()]);
+
 /* 
 //////////////////////////////
 //Nullish Coalescing Operator (??)   - ES 2020
@@ -168,6 +249,7 @@ const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 console.log(menu); //(7) ["Pizza", "Pasta", "Risotto", "Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
 
 //NOTE : Iterables : are arrays, strings, maps or Sets but not Objects
+
 
 const str = "Kean";
 const letters = [...str, " ", "D."];
